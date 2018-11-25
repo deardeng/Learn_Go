5e6fa3bec24e8738571a4300c4fab4ed1f0ebd15
deardeng <565620795@qq.com>
2018-11-25 20:18:50 +0800

    logagent env succ

---
+++ src/day11/tail/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/hpcloud/tail"
+)
+
+func main() {
+	//filename := "E:\\project\\kafka_2.12-0.11.0.0\\config\\server.properties"
+	filename := "D:\\zookeeper-3.4.10\\conf\\log4j.properties"
+	//filename := ".\\my.log"
+	tails, err := tail.TailFile(filename, tail.Config{
+		ReOpen: true,
+		Follow: true,
+		//Location:  &tail.SeekInfo{Offset: 0, Whence: 2},
+		MustExist: false,
+		Poll:      true,
+	})
+	if err != nil {
+		fmt.Println("tail file err:", err)
+		return
+	}
+	var msg *tail.Line
+	var ok bool
+	for true {
+		msg, ok = <-tails.Lines
+		if !ok {
+			fmt.Printf("tail file close reopen, filename:%s\n", tails.Filename)
+			time.Sleep(100 * time.Millisecond)
+			continue
+		}
+		fmt.Println("msg:", msg)
+	}
+}
